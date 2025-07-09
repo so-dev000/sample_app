1d3bf41659783e0d4a7093953a2ca1e42642900e
Nakamura <so.dev000@gmail.com>
2025-07-09 17:28:24 +0900

    fix test error

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
 //= link bootstrap.min.js
 //= link popper.js
