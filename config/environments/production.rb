f3210d2582e8f492154f4a28b717d286c2bc5508
Nakamura <so.dev000@gmail.com>
2025-07-13 21:10:12 +0900

    fix logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger($stdout)
+  config.logger   = ActiveSupport::TaggedLogging.new(Logger.new($stdout))
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
