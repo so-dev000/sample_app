60a27c4c7fbfd3148d7d219ead586ac5eec9ae74
Nakamura <so.dev000@gmail.com>
2025-07-14 15:17:51 +0900

    advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,8 @@
 module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
+    # セッションリプレイ攻撃から保護する
+    session[:session_token] = user.session_token
   end
 
   def remember(user)
@@ -11,7 +13,8 @@ module SessionsHelper
 
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      @current_user ||= User.find_by(id: user_id) if user && session[:session_token] == user.session_token
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user&.authenticated?(cookies[:remember_token])
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -76,7 +76,7 @@ end
 class RememberingTest < UsersLogin
   test 'login with remembering' do
     log_in_as(@user, remember_me: '1')
-    assert_not cookies[:remember_token].blank?
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
   end
 
   test 'login without remembering' do
@@ -84,6 +84,6 @@ class RememberingTest < UsersLogin
     log_in_as(@user, remember_me: '1')
     # Cookieが削除されていることを検証してからログイン
     log_in_as(@user, remember_me: '0')
-    assert cookies[:remember_token].blank?
+    assert_empty cookies[:remember_token]
   end
 end
