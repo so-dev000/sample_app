51ee93456a7c58d156ae590164dcc7c319e42372
Nakamura <so.dev000@gmail.com>
2025-07-17 14:23:19 +0900

    fix seeds to prevent email conflict

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,22 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-User.create!(name: 'Example User', email: 'example@railstutorial.org', password: 'password',
-             password_confirmation: 'password', admin: true, activated: true, activated_at: Time.zone.now)
+User.find_or_create_by!(email: 'example@railstutorial.org') do |user|
+  user.name = 'Example User'
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.admin = true
+  user.activated = true
+  user.activated_at = Time.zone.now
+end
 
 99.times do |n|
-  name = Faker::Name.name
   email = "example-#{n + 1}@railstutorial.org"
-  password = 'password'
-  User.create!(name: name, email: email, password: password, password_confirmation: password, activated: true,
-               activated_at: Time.zone.now)
+  User.find_or_create_by!(email: email) do |user|
+    user.name = Faker::Name.name
+    user.password = 'password'
+    user.password_confirmation = 'password'
+    user.activated = true
+    user.activated_at = Time.zone.now
+  end
 end
